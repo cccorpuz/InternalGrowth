92be43914d80d18e45a58c77e29f77fbdc73784c
Crispin Corpuz <ccorpuz@Crispins-MacBook-Pro.local>
2020-07-14 14:48:47 -0500

    First Commit

---
+++ InternalGrowth/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  InternalGrowth
+//
+//  Created by Crispin Corpuz on 7/14/20.
+//  Copyright © 2020 Crispin Corpuz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
